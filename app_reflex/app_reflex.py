ec1d73842a42c248a6c69341d3c5212deb3eca80
ingteceduardo <ingteceduardo@gmail.com>
2024-08-04 16:36:19 +0000

    cambio a navbar a usar

--- app_reflex/app_reflex.py
+++ app_reflex/app_reflex.py
@@ -5,58 +5,63 @@ from pathlib import Path
 
 file = Path(__file__).resolve()
 parent, root = file.parent, file.parents[1]
+print(root)
 sys.path.append(str(root))
 
 import reflex as rx
 from rxconfig import config
 
+from app_reflex.components.navbar import navbar_icons, navbar_icons_item
 
 class State(rx.State):
     """The app state."""
 
-def navbar_link(text: str, url: str) -> rx.Component:
-    return rx.link(
-        rx.text(text, size="4", weight="medium"), href=url
-    )
+# def navbar_link(text: str, url: str) -> rx.Component:
+#     return rx.link(
+#         rx.text(text, size="4", weight="medium"), href=url
+#     )
 
-def navbar() -> rx.Component:
-    return rx.box(
+# def navbar() -> rx.Component:
+#     return rx.box(
         
-        rx.hstack(
-            rx.hstack(
-                rx.image(
-                    src="/logo.png",
-                    width="2.25em",
-                    height="auto",
-                    border_radius="25%",
-                ),
-                rx.heading(
-                    "Trading", size="7", weight="bold"
-                ),
-                align_items="center",
-            ),
-            rx.hstack(
-                navbar_link("Home", "/#"),
-                navbar_link("About", "/#"),
-                navbar_link("Pricing", "/#"),
-                navbar_link("Contact", "/#"),
-                justify="end",
-                spacing="5",
-            ),
-            justify="between",
-            align_items="center",
-        ),
-    bg=rx.color("accent", 3),
-    padding="1em",
-    # position="fixed",
-    # top="0px",
-    # z_index="5",
-    width="100%",
-)
+#         rx.hstack(
+#             rx.hstack(
+#                 rx.image(
+#                     src="/logo.png",
+#                     width="2.25em",
+#                     height="auto",
+#                     border_radius="25%",
+#                 ),
+#                 rx.heading(
+#                     "Trading", size="7", weight="bold"
+#                 ),
+#                 align_items="center",
+#             ),
+#             rx.hstack(
+#                 navbar_link("Home", "/#"),
+#                 navbar_link("About", "/#"),
+#                 navbar_link("Pricing", "/#"),
+#                 navbar_link("Contact", "/#"),
+#                 justify="end",
+#                 spacing="5",
+#             ),
+#             justify="between",
+#             align_items="center",
+#         ),
+#     bg=rx.color("accent", 3),
+#     padding="1em",
+#     # position="fixed",
+#     # top="0px",
+#     # z_index="5",
+#     width="100%",
+# )
+
+
+
 
 def index() -> rx.Component:
     return rx.box(
-        navbar(),
+        navbar_icons(),
     )
 
 
---
+++ app_reflex/components/navbar.py
@@ -0,0 +1,51 @@
+import reflex as rx
+
+def navbar_icons_item(
+    text: str, icon: str, url: str
+) -> rx.Component:
+    return rx.link(
+        rx.hstack(
+            rx.icon(icon),
+            rx.text(text, size="4", weight="medium"),
+        ),
+        href=url,
+    )
+
+def navbar_icons() -> rx.Component:
+    return rx.box(
+        rx.hstack(
+            rx.hstack(
+                rx.image(
+                    src="/logo.png",
+                    width="2.25em",
+                    height="auto",
+                    border_radius="25%",
+                ),
+                rx.heading(
+                    "Reflex", size="7", weight="bold"
+                ),
+                align_items="center",
+            ),
+            rx.hstack(
+                navbar_icons_item("Home", "home", "/#"),
+                navbar_icons_item(
+                    "Pricing", "coins", "/#"
+                ),
+                navbar_icons_item(
+                    "Contact", "mail", "/#"
+                ),
+                navbar_icons_item(
+                    "Services", "layers", "/#"
+                ),
+                spacing="6",
+            ),
+            justify="between",
+            align_items="center",
+        ),
+        bg=rx.color("accent", 3),
+        padding="1em",
+        # position="fixed",
+        # top="0px",
+        # z_index="5",
+        width="100%",
+    )
